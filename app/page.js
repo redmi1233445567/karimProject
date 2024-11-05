c91a2846261031f5e394b43a66a6a702ce68592a
redmi <hmadaredmi7@gmail.com>
2024-11-05 16:42:59 +0200

    first commit

---
+++ app/page.js
@@ -0,0 +1,12 @@
+import Business from "./componants/Business";
+import HomePage from "./componants/Home";
+
+
+export default function Home() {
+  return (
+    <div>
+      <HomePage />
+      <Business />
+    </div>
+  );
+}
